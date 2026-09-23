2cf52f0476b8e08511ded9214e49dbf21ce91e5c
RajdeepSingh parmar <rajdeepsinghparmar.142893@marwadiuniversity.ac.in>
2026-09-23 15:57:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (64.70%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-        int half = 0;
-        while (half < x) {          // stop when half >= remaining x
+        int original =x;
+        int long half = 0;
+        while (x!=0) {          // stop when half >= remaining x
             half = half * 10 + x % 10;
             x /= 10;
         }
         // even length: half == x ; odd length: middle digit drops off
-        return half == x || half / 10 == x;
+        return original==half;
     }
 };
